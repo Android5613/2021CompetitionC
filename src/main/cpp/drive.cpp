6e7d504677a12028e4c6267cce1387a92306a866
Android5613 <Yuengera@gmail.com>
2021-03-02 16:16:16 -0900

    Added deadzones for robot drive.

--- src/main/cpp/drive.cpp
+++ src/main/cpp/drive.cpp
@@ -3,10 +3,15 @@
 // This is the drive function to drive the robot.
 void Robot::arcade_drive() {
   
-  // Grabs values from the joysticks, multiplies them by a constant, and stores them in a variable.
-  // To change the sensitivity, change the constant.
-  double speed = DriveStick.GetY();
-  double turn = DriveStick.GetZ() * -.45;
+  double speed = 0;
+  double turn = 0;
+  
+  if (DriveStick.GetY() >=0.05) {
+    double speed = DriveStick.GetY();
+  } 
+  if (DriveStick.GetZ() >=0.05) {
+    double turn = DriveStick.GetZ() * -.45;
+  } 
   
   // Sets the motors to be the speed with the turn value.
   double left = speed+turn;
